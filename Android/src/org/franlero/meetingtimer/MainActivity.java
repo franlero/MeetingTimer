4e05e95e4f9d12f298df73da3ecd7cf8b9f78ac9
François Le Rolland <francois.lerolland@gmail.com>
2013-04-21 17:46:46 +0200

    First activity with seekbar

--- Android/src/org/franlero/meetingtimer/MainActivity.java
+++ Android/src/org/franlero/meetingtimer/MainActivity.java
@@ -3,13 +3,26 @@ package org.franlero.meetingtimer;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnSeekBarChangeListener {
+	
+	private SeekBar bar;
+	
+	private EditText duration;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		bar = (SeekBar)findViewById(R.id.seekBar1);
+		bar.setOnSeekBarChangeListener(this);
+		
+		duration = (EditText)findViewById(R.id.editText1);
+		duration.setText(Integer.toString(bar.getProgress()));
 	}
 
 	@Override
@@ -17,6 +30,25 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+		// TODO Auto-generated method stub
+    	// change progress text label with current seekbar value
+		duration.setText(Integer.toString(progress));	
+	}
+
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
